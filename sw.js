3478950f67ff2a50e216decff57da7f3441262e3
pcm-coaching-center <hasanuzzamanmd585@gmail.com>
2026-09-15 11:03:06 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='pcm-coaching-center-v1';
+const ASSETS=['./','./PCM_Coaching_Center.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
